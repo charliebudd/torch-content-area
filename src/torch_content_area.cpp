c4f7152f49286e42017a7e50e65a7d967098a7fc
Charlie Budd <charles.budd@kcl.ac.uk>
2022-02-17 15:22:59 +0000

    adding testing
    Signed-off-by: Charlie Budd <charles.budd@kcl.ac.uk>

--- src/torch_content_area.cpp
+++ src/torch_content_area.cpp
@@ -32,8 +32,42 @@ py::tuple area_to_tuple(Area area)
     }
 }
 
-py::tuple get_area(torch::Tensor image) 
+Area tuple_to_area(py::tuple tuple)
 {
+    std::string area_type = tuple[0].cast<std::string>();
+    py::tuple area_info = tuple[1];
+
+    if (area_type == "Circle")
+    {
+        Area area;
+        area.type = Area::Circle;
+        area.circle.x = area_info[0].cast<uint>();
+        area.circle.y = area_info[1].cast<uint>();
+        area.circle.r = area_info[2].cast<uint>();
+        return area;
+    }
+    else if (area_type == "Rectangle")
+    {
+        Area area;
+        area.type = Area::Rectangle;
+        area.rectangle.x = area_info[0].cast<uint>();
+        area.rectangle.y = area_info[1].cast<uint>();
+        area.rectangle.w = area_info[2].cast<uint>();
+        area.rectangle.h = area_info[3].cast<uint>();
+        return area;
+    }
+    else
+    {
+        Area area;
+        area.type = Area::None;
+        return area;
+    }
+}
+
+py::tuple infer_area(torch::Tensor image) 
+{
+    image = image.contiguous();
+
     uint height = image.size(1);
     uint width = image.size(2);
 
@@ -42,7 +76,21 @@ py::tuple get_area(torch::Tensor image)
     return area_to_tuple(area);
 }
 
-torch::Tensor get_area_mask(torch::Tensor image)
+torch::Tensor draw_mask(torch::Tensor image, py::tuple area_tuple) 
+{
+    uint height = image.size(1);
+    uint width = image.size(2);
+
+    Area area = tuple_to_area(area_tuple);
+
+    torch::Tensor mask = torch::empty_like(image[0]);
+    draw_area_cuda(area, mask.data_ptr<uint8>(), height, width);
+
+    return mask;
+}
+
+
+torch::Tensor infer_mask(torch::Tensor image)
 {
     image = image.contiguous();
 
@@ -50,7 +98,6 @@ torch::Tensor get_area_mask(torch::Tensor image)
     uint width = image.size(2);
 
     torch::Tensor mask = torch::empty_like(image[0]);
-
     Area area = infer_area_cuda(image.data_ptr<uint8>(), height, width, 8);
     draw_area_cuda(area, mask.data_ptr<uint8>(), height, width);
 
@@ -59,6 +106,7 @@ torch::Tensor get_area_mask(torch::Tensor image)
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) 
 {
-    m.def("get_area", &get_area, "Infers the conent area for a given endoscopic image");
-    m.def("get_area_mask", &get_area_mask, "Infers the conent area for a given endoscopic image and returns a binary mask");
+    m.def("infer_area", &infer_area, "Infers the conent area for a given endoscopic image");
+    m.def("draw_mask", &draw_mask, "Draws the provided content area");
+    m.def("infer_mask", &infer_mask, "Infers the conent area for a given endoscopic image and returns a binary mask");
 }
