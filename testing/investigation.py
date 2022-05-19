7981f6f032656d6db05b4a92fa5b61f7abc439af
charliebudd <charles.budd@kcl.ac.uk>
2022-05-19 14:22:52 +0100

    fixing tests

--- testing/investigation.py
+++ testing/investigation.py
@@ -1,8 +1,9 @@
 from torchcontentarea import ContentAreaInference
-from testing.utils import TestDataset, perimeter_distance_score
+from utils import TestDataset, perimeter_distance_score
 import matplotlib.pyplot as plt
 
-def show(img, gt_area, area, points_x, points_y, norm_x, norm_y):
+
+def show_image(img, gt_area, area, points_x, points_y, norm_x, norm_y):
     norm_y = [-y for y in norm_y]
     plt.rcParams["figure.figsize"] = (15, 10)
     plt.subplot(111).axis("off")
@@ -19,6 +20,31 @@ def show(img, gt_area, area, points_x, points_y, norm_x, norm_y):
     plt.ylim((img.shape[1], 0))
     plt.show()
 
+
+def show_score_histograms(area_scores, no_area_scores, threshold_value):
+    area_border_scores, area_circle_scores, area_final_scores = zip(*area_scores)
+    no_area_border_scores, no_area_circle_scores, no_area_final_scores = zip(*no_area_scores)
+    plt.rcParams["figure.figsize"] = (15, 5)
+    plt.subplot(131)
+    plt.title("Border Score")
+    plt.ylabel("Sample Count")
+    plt.xlabel("Score")
+    plt.hist([area_border_scores, no_area_border_scores], bins=20)
+    plt.xlim(0, 1)
+    plt.subplot(132)
+    plt.title("Circle Score")
+    plt.xlabel("Score")
+    plt.hist([area_circle_scores, no_area_circle_scores], bins=20)
+    plt.xlim(0, 1)
+    plt.subplot(133)
+    plt.title("Final Score")
+    plt.xlabel("Score")
+    plt.hist([area_final_scores, no_area_final_scores], bins=20)
+    plt.axvline(threshold_value, color="red")
+    plt.xlim(0, 1)
+    plt.show()
+
+
 content_area = ContentAreaInference()
 dataset = TestDataset()
 
@@ -28,8 +54,8 @@ show_bad_classification = True
 area_scores = []
 no_area_scores = []
 
-correct_scores = []
-incorrect_scores = []
+area_scores_incorrect = []
+no_area_scores_incorrect = []
 
 for index, (img, gt_area) in enumerate(dataset):
 
@@ -44,69 +70,25 @@ for index, (img, gt_area) in enumerate(dataset):
         if show_bad_circles and score > 10:
             print(f"Bad Circle...")
             print(f"Error: {int(score)}px")
-            show(img, gt_area, area, points_x, points_y, norm_x, norm_y)
+            show_image(img, gt_area, area, points_x, points_y, norm_x, norm_y)
 
     if show_bad_classification and expect_area != area_found:
         print(f"False Positive..." if area_found else "False Negative...")
         print(f"Border Score: {confidence_scores[0]:.3f}")
         print(f"Circle Score: {confidence_scores[1]:.3f}")
         print(f"Final Score: {confidence_scores[2]:.3f}")
-        show(img, gt_area, area, points_x, points_y, norm_x, norm_y)
+        show_image(img, gt_area, area, points_x, points_y, norm_x, norm_y)
 
     if expect_area:
         area_scores.append(confidence_scores)
     else:
         no_area_scores.append(confidence_scores)
         
-    if expect_area == area_found:
-        correct_scores.append(confidence_scores)
-    else:
-        incorrect_scores.append(confidence_scores)
+    if expect_area != area_found:
+        if expect_area:
+            area_scores_incorrect.append(confidence_scores)
+        else:
+            no_area_scores_incorrect.append(confidence_scores)
 
-
-area_border_scores, area_circle_scores, area_final_scores = zip(*area_scores)
-no_area_border_scores, no_area_circle_scores, no_area_final_scores = zip(*no_area_scores)
-
-plt.rcParams["figure.figsize"] = (15, 5)
-plt.subplot(131)
-plt.title("Border Score")
-plt.ylabel("Sample Count")
-plt.xlabel("Score (0-1)")
-plt.hist([area_border_scores, no_area_border_scores], bins=20)
-plt.xlim(0, 1)
-plt.subplot(132)
-plt.title("Circle Score")
-plt.xlabel("Score (0-1)")
-plt.hist([area_circle_scores, no_area_circle_scores], bins=20)
-plt.xlim(0, 1)
-plt.subplot(133)
-plt.title("Final Score")
-plt.xlabel("Score (0-1)")
-plt.hist([area_final_scores, no_area_final_scores], bins=20)
-plt.plot([0.15, 0.15], [0, 1400], color="red")
-plt.xlim(0, 1)
-plt.show()
-
-
-correct_border_scores, correct_circle_scores, correct_final_scores = zip(*correct_scores)
-incorrect_border_scores, incorrect_circle_scores, incorrect_final_scores = zip(*incorrect_scores)
-
-plt.rcParams["figure.figsize"] = (15, 5)
-plt.subplot(131)
-plt.title("Border Score")
-plt.ylabel("Sample Count")
-plt.xlabel("Score (0-1)")
-plt.hist([incorrect_border_scores], bins=20)
-plt.xlim(0, 1)
-plt.subplot(132)
-plt.title("Circle Score")
-plt.xlabel("Score (0-1)")
-plt.hist([incorrect_circle_scores], bins=20)
-plt.xlim(0, 1)
-plt.subplot(133)
-plt.title("Final Score")
-plt.xlabel("Score (0-1)")
-plt.hist([incorrect_final_scores], bins=20)
-plt.plot([0.15, 0.15], [0, 1400], color="red")
-plt.xlim(0, 1)
-plt.show()
+show_score_histograms(area_scores, no_area_scores, 0.15)
+show_score_histograms(area_scores_incorrect, no_area_scores_incorrect, 0.15)
