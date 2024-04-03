f678221acc64f8221319cf1383bd518ad971dc24
charliebudd <charles.budd@kcl.ac.uk>
2024-04-03 10:55:46 +0100

    python implementation

--- src/torchcontentarea/pythonimplementation/fit_area.py
+++ src/torchcontentarea/pythonimplementation/fit_area.py
@@ -1,8 +1,135 @@
 import torch
+from math import sqrt
 from typing import Sequence
+
+MAX_CENTER_DIST=0.2
+MIN_RADIUS=0.2
+MAX_RADIUS=0.8
+RANSAC_ATTEMPTS=32
+RANSAC_ITERATIONS=3
+RANSAC_INLIER_THRESHOLD=3
+
+def check_circle(x, y, r, w, h):
+
+    x_diff = x - 0.5 * w
+    y_diff = y - 0.5 * h
+    diff = sqrt(x_diff * x_diff + y_diff * y_diff)
+
+    valid = True
+    valid &= diff < MAX_CENTER_DIST * w
+    valid &= r > MIN_RADIUS * w
+    valid &= r < MAX_RADIUS * w
+    
+    return valid
+
+def calculate_circle(points):
+    
+    ax, ay = points[:2, 0]
+    bx, by = points[:2, 1]
+    cx, cy = points[:2, 2]
+    
+    offset = bx * bx + by * by
+
+    bc = 0.5 * (ax * ax + ay * ay - offset)
+    cd = 0.5 * (offset - cx * cx - cy * cy)
+
+    det = (ax - bx) * (by - cy) - (bx - cx) * (ay - by)
+ 
+    if abs(det) > 1e-8:
+        idet = 1.0 / det
+        x = (bc * (by - cy) - cd * (ay - by)) * idet
+        y = (cd * (ax - bx) - bc * (bx - cx)) * idet
+        r = sqrt((bx - x) * (bx - x) + (by - y) * (by - y))
+        return x, y, r
+    else:
+        return None
+
+def get_circle(points):
+    
+    if points.size(1) == 3:
+        return calculate_circle(points)
+    else:
+        lhs = torch.zeros(3, 3).to(points.device)
+        rhs = torch.zeros(3).to(points.device)
+
+        for p_x, p_y, _ in points.T:
+            lhs[0, 0] += p_x * p_x
+            lhs[0, 1] += p_x * p_y
+            lhs[1, 1] += p_y * p_y
+            lhs[0, 2] += p_x
+            lhs[1, 2] += p_y
+            lhs[2, 2] += 1
+
+            rhs[0] += p_x * p_x * p_x + p_x * p_y * p_y
+            rhs[1] += p_x * p_x * p_y + p_y * p_y * p_y
+            rhs[2] += p_x * p_x + p_y * p_y
+
+        lhs[1, 0] = lhs[0, 1]
+        lhs[2, 0] = lhs[0, 2]
+        lhs[2, 1] = lhs[1, 2]
+
+        L = torch.linalg.cholesky(lhs)
+        y = torch.linalg.solve(L, rhs)
+        x = torch.linalg.solve(L.T, y)
+        
+        A, B, C = x[0], x[1], x[2]
+
+        x = A / 2.0
+        y = B / 2.0
+        r = torch.sqrt(4.0 * C + A * A + B * B) / 2.0
+        
+        return x, y, r
 
 def fit_area(points: torch.Tensor, image_size: Sequence[int], confidence_thresholds: Sequence[float]=(0.03, 0.06)) -> torch.Tensor:
     """
     Finds candidate edge points and corresponding scores in the given image(s) using handcrafted feature extraction.
     """
-    raise NotImplementedError()
+
+    batched = len(points.shape) == 3
+    
+    if not batched:
+        points = points.unsqueeze(0)
+
+    areas = []
+
+    for point_batch in points:
+        
+        point_batch = point_batch[:, point_batch[2] > confidence_thresholds[0]]
+        
+        if point_batch.size(1) < 3:
+            areas.append(torch.zeros(4))
+            continue
+        
+        best_circle = torch.zeros(4)
+
+        for _ in range(RANSAC_ATTEMPTS):
+             
+            indices = torch.randperm(point_batch.size(1))[:3]
+            inliers = point_batch[:, indices]
+
+            for _ in range(RANSAC_ITERATIONS):
+                x, y, r = get_circle(inliers)
+                
+                dx = x - point_batch[0]
+                dy = y - point_batch[1]
+                
+                error = torch.abs(torch.sqrt(dx**2 + dy**2) - r)
+                
+                inliers = point_batch[:, error < RANSAC_INLIER_THRESHOLD]
+                circle_score = inliers[2].sum()
+                
+            circle_score /= point_batch.size(1)
+
+            circle_valid = check_circle(x, y, r, image_size[1], image_size[0])
+
+            if circle_valid and circle_score > best_circle[3]:
+                best_circle = torch.tensor([x, y, r, circle_score])
+                
+        areas.append(best_circle)
+                
+    areas = torch.stack(areas)
+    
+    if not batched:
+        areas = areas[0]
+
+    return areas
--- src/torchcontentarea/pythonimplementation/get_points.py
+++ src/torchcontentarea/pythonimplementation/get_points.py
@@ -1,14 +1,94 @@
 import torch
 from typing import Sequence, Optional
+    
+DEG2RAD=0.01745329251
+RAD2DEG=1.0/DEG2RAD
+
+GRAY_SCALE_WEIGHTS = torch.tensor([
+    0.2126, 0.7152, 0.0722
+])
+
+SOBEL_KERNEL = torch.tensor([
+    [[
+        [-0.25, 0.0, 0.25],
+        [-0.5, 0.0, 0.5],
+        [-0.25, 0.0, 0.25],
+    ]],
+    [[
+        [-0.25, -0.5, -0.25],
+        [0.0, 0.0, 0.0],
+        [0.25, 0.5, 0.25],
+    ]],
+])
 
 def get_points_handcrafted(image: torch.Tensor, strip_count: int=16, feature_thresholds: Sequence[float]=(20, 30, 25)) -> torch.Tensor:
     """
     Finds candidate edge points and corresponding scores in the given image(s) using handcrafted feature extraction.
     """
-    raise NotImplementedError()
+    
+    batched = len(image.shape) == 4
+    device = image.device
+    if not batched:
+        image.unsqueeze(0)
+    if image.dtype.is_floating_point:
+        image = image * 255.0
+    if image.size(1) != 1:
+        image = (image * GRAY_SCALE_WEIGHTS[None, :, None, None]).sum(dim=1)
+    image = image.float()
+    B, H, W = image.shape
+
+    strip_indices = torch.arange(strip_count, device=device)
+    strip_heights = (1 + (H - 2) / (1.0 + torch.exp(-(strip_indices - strip_count / 2.0 + 0.5) / (strip_count / 8.0)))).long()
+    
+    indices = torch.cat([strip_heights-1, strip_heights, strip_heights+1])
+    strips = image[:, indices, :].reshape(B, 3, strip_count, W).permute(0, 2, 1, 3)
+    strips = torch.cat([strips[..., :W//2], strips.flip(-1)[..., :W//2]], dim=1)
+    
+    ys = torch.cat([strip_heights, strip_heights])[None, :, None].repeat(B, 1, W//2-2)
+    xs = (torch.arange(W//2-2) + 1)[None, None, :].repeat(B, strip_count, 1)
+    xs = torch.cat([xs, W - 1 - xs], dim=1)
+    
+    grad = torch.conv2d(strips.reshape(B*2*strip_count, 1, 3, -1), SOBEL_KERNEL.to(device)).reshape(B, 2*strip_count, 2, -1)
+    strips = strips[:, :, 1, 1:-1]
+    
+    grad_x = grad[:, :, 0]
+    grad_y = grad[:, :, 1]
+    grad_x[:, strip_count:] *= -1
+    
+    grad = torch.sqrt(grad_x**2 + grad_y**2)
+    
+    max_preceeding_intensity = torch.cummax(strips, dim=-1)[0]
+    
+    center_dir_x = (0.5 * W) - xs
+    center_dir_y = (0.5 * H) - ys
+    center_dir_norm = torch.sqrt(center_dir_x * center_dir_x + center_dir_y * center_dir_y)
+
+    dot = torch.where(grad == 0, -1, (center_dir_x * grad_x + center_dir_y * grad_y) / (center_dir_norm * grad))
+    dot = torch.clamp(dot, -0.99, 0.99)
+    angle = RAD2DEG * torch.acos(dot)
+    
+    edge_score = torch.tanh(grad / feature_thresholds[0])
+    angle_score = 1.0 - torch.tanh(angle / feature_thresholds[1])
+    intensity_score = 1.0 - torch.tanh(max_preceeding_intensity / feature_thresholds[2])
+    
+    
+    point_scores = edge_score * angle_score * intensity_score
+    
+    point_scores, indices = torch.max(point_scores, dim=-1)
+    
+    point_scores = point_scores.squeeze()
+    ys = torch.gather(ys, 2, indices[:, :, None]).squeeze()
+    xs = torch.gather(xs, 2, indices[:, :, None]).squeeze()
+    
+    result = torch.stack([xs, ys, point_scores], dim=1).reshape(B, strip_count*2, 3).permute(0, 2, 1)
+    
+    if not batched:
+        result = result[0]
+    
+    return result
 
 def get_points_learned(image: torch.Tensor, strip_count: int=16, model: Optional[torch.jit.ScriptModule]=None,  model_patch_size: int=7) -> torch.Tensor:
     """
     Finds candidate edge points and corresponding scores in the given image(s) using learned feature extraction.
     """
-    raise NotImplementedError()
+    raise NotImplementedError("The learned method is not implemented in the python fallback. The handcrafted method should provide better performance, but to use the learned method you will need to install the compiled extension.")
